afcad1f0a2344f6e16099f1aa94b0691c9471d9e
Jinwon <vincent841@naver.com>
2021-03-31 14:43:19 +0900

    [robotics] update the configuraiton file to launch python modules

--- packages/operato-robotics/installer/config.production.js
+++ packages/operato-robotics/installer/config.production.js
@@ -1,31 +1,31 @@
 module.exports = {
-  subdomain: 'system',
-  SECRET: '0xD58F835B69D207A76CC5F84a70a1D0d4C79dAC95', // temporary...
+  subdomain: "system",
+  SECRET: "0xD58F835B69D207A76CC5F84a70a1D0d4C79dAC95", // temporary...
   email: {
-    host: 'smtp.office365.com', // your sender-email smtp host
+    host: "smtp.office365.com", // your sender-email smtp host
     port: 587, // smtp server port
     secure: false, // true for 465, false for other ports
     auth: {
-      user: 'your sender-email',
-      pass: 'your sender-email password' // generated ethereal password
+      user: "your sender-email",
+      pass: "your sender-email password", // generated ethereal password
     },
     secureConnection: false,
     tls: {
-      ciphers: 'SSLv3'
-    }
+      ciphers: "SSLv3",
+    },
   },
   logger: {
     file: {
-      filename: 'logs/application-%DATE%.log',
-      datePattern: 'YYYY-MM-DD-HH',
+      filename: "logs/application-%DATE%.log",
+      datePattern: "YYYY-MM-DD-HH",
       zippedArchive: false,
-      maxSize: '20m',
-      maxFiles: '2d',
-      level: 'info'
+      maxSize: "20m",
+      maxFiles: "2d",
+      level: "info",
     },
     console: {
-      level: 'silly'
-    }
+      level: "silly",
+    },
   },
   //   ormconfig: {
   //     name: 'default',
@@ -36,36 +36,55 @@ module.exports = {
   //     logger: 'debug'
   //   },
   ormconfig: {
-    name: 'default',
-    type: 'postgres',
-    host: 'postgres',
+    name: "default",
+    type: "postgres",
+    host: "postgres",
     port: 5432,
-    database: 'postgres',
-    username: 'postgres',
-    password: 'abcd1234',
+    database: "postgres",
+    username: "postgres",
+    password: "abcd1234",
     synchronize: true,
-    logging: true
+    logging: true,
   },
   vision: {
     streamingPort: 3001,
     camera: {
       cameraCalibrator: {
-        program: ['python3', '/app/object-tracker-python/object_tracker/calibcamera_engine.py']
+        program: [
+          "python3",
+          "/app/object-tracker-python/object_tracker/objtracking_entry.py",
+          "CAMERACALIB",
+        ],
       },
       handEyeCalibrator: {
-        program: ['python3', '/app/object-tracker-python/object_tracker/calibhandeye_engine.py']
+        program: [
+          "python3",
+          "/app/object-tracker-python/object_tracker/objtracking_entry.py",
+          "HANDEYECALIB",
+        ],
       },
       ROIDetector: {
-        program: ['python3', '/app/object-tracker-python/object_tracker/roi_engine.py']
-      }
+        program: [
+          "python3",
+          "/app/object-tracker-python/object_tracker/roi_engine.py",
+        ],
+      },
     },
     robotArm: {
       markerOffsetCalibrator: {
-        program: ['python3', '/app/object-tracker-python/object_tracker/objecttracking_engine.py']
-      }
+        program: [
+          "python3",
+          "/app/object-tracker-python/object_tracker/objtracking_entry.py",
+          "OBJTRACKING",
+        ],
+      },
     },
     objectTracker: {
-      program: ['python3', '/app/object-tracker-python/object_tracker/objecttracking_engine.py']
-    }
-  }
-}
+      program: [
+        "python3",
+        "/app/object-tracker-python/object_tracker/objtracking_entry.py",
+        "OBJTRACKING",
+      ],
+    },
+  },
+};
