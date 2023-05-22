605f06e2f58b58cc946c9481c48e4a7d04443edb
Jinwon <41100208+vincent841@users.noreply.github.com>
2023-05-22 20:29:41 +0900

    update docker files of both board & ecs

--- packages/operato-ecs/installer/config.production.js
+++ packages/operato-ecs/installer/config.production.js
@@ -1,49 +1,53 @@
 module.exports = {
-  subdomain: 'system',
-  SECRET: '0xD58F835B69C217A76CA5FA4a70a1D0d4D79dAC64', // temporary...
+  subdomain: "system",
+  SECRET: "0xD58F835B69C217A76CA5FA4a70a1D0d4D79dAC64", // temporary...
   email: {
-    host: 'smtp.office365.com', // your sender-email smtp host
+    host: "smtp.office365.com", // your sender-email smtp host
     port: 587, // smtp server port
     secure: false, // true for 465, false for other ports
     auth: {
-      user: 'your sender-email',
-      pass: 'your sender-email password' // generated ethereal password
+      user: "your sender-email",
+      pass: "your sender-email password", // generated ethereal password
     },
     secureConnection: false,
     tls: {
-      ciphers: 'SSLv3'
-    }
+      ciphers: "SSLv3",
+    },
   },
   logger: {
     file: {
-      filename: 'logs/application-%DATE%.log',
-      datePattern: 'YYYY-MM-DD-HH',
+      filename: "logs/application-%DATE%.log",
+      datePattern: "YYYY-MM-DD-HH",
       zippedArchive: false,
-      maxSize: '20m',
-      maxFiles: '2d',
-      level: 'info'
+      maxSize: "20m",
+      maxFiles: "2d",
+      level: "info",
     },
     console: {
-      level: 'silly'
-    }
+      level: "silly",
+    },
   },
-  //   ormconfig: {
-  //     name: 'default',
-  //     type: 'sqlite',
-  //     database: 'db.sqlite',
-  //     synchronize: false,
-  //     logging: true,
-  //     logger: 'debug'
-  //   },
   ormconfig: {
-    name: 'default',
-    type: 'postgres',
-    host: 'postgres',
+    name: "default",
+    type: "postgres",
+    host: "postgres",
     port: 5432,
-    database: 'postgres',
-    username: 'postgres',
-    password: 'abcd1234',
+    database: "postgres",
+    username: "postgres",
+    password: "abcd1234",
     synchronize: true,
-    logging: true
-  }
-}
+    logging: true,
+  },
+  scheduler: {
+    /* Name to be used for setting client's "application" properties when registering a schedule */
+    application: "operato-ecs",
+    /* Base endpoint to be used for setting "callback" properties when registering a schedule */
+    callbackBase: "http://app:3000",
+    /* 
+        Scheduler service endpoint
+        caution) endpoint for "localhost" has some problem. 
+        https://github.com/node-fetch/node-fetch/issues/1624
+      */
+    endpoint: "http://scheduler:9902",
+  },
+};
