ad9ef3492bc8279ad2eaff2b98bf7d027665e1a4
Jinwon <41100208+vincent841@users.noreply.github.com>
2023-05-22 20:38:53 +0900

    fix installation issue of both ecs & board

--- packages/operato-ecs/installer/install.sh
+++ packages/operato-ecs/installer/install.sh
@@ -40,6 +40,13 @@ else
   curl -O https://raw.githubusercontent.com/things-factory/things-factory/master/packages/operato-ecs/installer/docker-compose.yml
 fi
 
+if [ -f "scheduler-config.yaml" ] ; then
+  echo "scheduler-config.yaml exist"
+else
+  echo "scheduler-config.yaml create"
+  curl -O https://raw.githubusercontent.com/things-factory/things-factory/master/packages/operato-board/installer/scheduler-config.yaml
+fi
+
 chmod u+x start.sh
 chmod u+x stop.sh
 chmod u+x upgrade.sh
